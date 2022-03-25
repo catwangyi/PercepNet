9492c48986ccee228690c0c3bb2f27320e9bd3d3
catwangyi <www.1121995621@qq.com>
2022-03-25 17:58:42 +0800

    2022.3.25

--- utils/utils.py
+++ utils/utils.py
@@ -1,12 +1,50 @@
 import librosa
+import numpy as np
 import torchaudio
 import torch
 import librosa.display
 import matplotlib.pyplot as plt
 
 
-def pitch_period_estimation(frame):
-    pass
+def auto_correlation(signal, clipped_signal = None):
+    '''
+    :param signal:
+    :return:
+    '''
+    if clipped_signal is None:
+        clipped_signal = signal
+    auto_corr = np.empty_like(signal)
+    for i in auto_corr:
+
+
+
+
+    return auto_corr
+
+
+def center_clipping(signal):
+    '''
+    :param signal:
+    :return:
+    '''
+    # signal = np.array(((2, -1, 4), (2, -5, 3)))
+    threshold = np.max(np.abs(signal)) * 0.68
+    signal = np.where(np.abs(signal) <= threshold, 0, signal)
+    signal = np.where(signal > threshold, 1, signal)
+    # print(signal)
+    signal = np.where(signal < -threshold, -1, signal)
+    # print(signal)
+    clipped_signal = signal
+    return clipped_signal
+
+
+def pitch_period_estimation(signal):
+    pitch_period = signal
+    '''
+    :param signal: ndarray
+    :return:
+    '''
+    return pitch_period
 
 def pitch_coherence(spectrogram):
     pass
@@ -18,6 +56,11 @@ def ERB_Bands(spectrogram):
 
 if __name__ == "__main__":
     signal, sr = librosa.load('D:\PercepNet\\audio\p226_001.wav', sr=None)
+    cliped_signal = center_clipping(signal[:1024])
+
+
+
+
     signal = torch.from_numpy(signal)
     spec_func = torchaudio.transforms.Spectrogram(n_fft=1024)
     signal_spectrogram = spec_func(signal)
